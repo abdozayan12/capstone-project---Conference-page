061a4b1dea0aaeccf9e825a5c5112912df7e850d
github_username <email_address>
2023-05-23 18:46:49 +0300

    about page desktop

--- data.js
+++ data.js
@@ -10,47 +10,47 @@ const speakersData = [
 
   {
     id: '02',
-    name: 'Yochai Benkler',
-    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    name: 'Kilnam Chon',
+    role: 'Berkman Professor',
     description:
-          'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-    featuredImage: 'images/speaker_01 1.svg',
+          'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame ',
+    featuredImage: 'images/speaker_01 1 (1).svg',
   },
 
   {
     id: '03',
-    name: 'Yochai Benkler',
-    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    name: 'SohYeong Noh',
+    role: 'Director of Art Centre Nabi and a board member of CC Korea',
     description:
-          'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-    featuredImage: 'images/speaker_01 1.svg',
+          'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    featuredImage: 'images/speaker_01 1 (2).svg',
   },
 
   {
     id: '04',
-    name: 'Yochai Benkler',
-    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    name: 'Julia Leda',
+    role: 'President of Young Pirates of Europe',
     description:
-          'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-    featuredImage: 'images/speaker_01 1.svg',
+          'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+    featuredImage: 'images/speaker_01 1 (3).svg',
   },
 
   {
     id: '05',
-    name: 'Yochai Benkler',
-    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    name: 'Lila tretikov',
+    role: 'Executive Director of the Wikimedia Foundation',
     description:
-          'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-    featuredImage: 'images/speaker_01 1.svg',
+          'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
+    featuredImage: 'images/speaker_01 1 (4).svg',
   },
 
   {
     id: '06',
-    name: 'Yochai Benkler',
-    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    name: 'Ryan Merkley',
+    role: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
     description:
-          'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-    featuredImage: 'images/speaker_01 1.svg',
+          'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment',
+    featuredImage: 'images/speaker_01 1 (5).svg',
   },
 ];
 
@@ -59,7 +59,7 @@ const speaker = document.querySelector('.featured-speakers');
 let output = ' ';
 speakersData.forEach((speaker) => {
   output += `
-    
+      <div class="grid">
         <img src="${speaker.featuredImage}" alt="first speaker">
         <ul>
             <li>
@@ -72,8 +72,10 @@ speakersData.forEach((speaker) => {
                 <p class="p2">${speaker.description}</p>
             </li>
         </ul>
-    
+      </div>  
       `;
 });
 
 speaker.innerHTML = output;
+
+
