6e8bc6a7c75eb8a21016f1c2e78cc775a86cf7d5
github_username <email_address>
2023-05-24 18:46:07 +0300

    fix required changes

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ const speakersData = [
   {
     id: '01',
     name: 'Yochai Benkler',
+    line: 'images/title_underline.png',
     role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
     description:
         'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
@@ -12,6 +13,7 @@ const speakersData = [
     id: '03',
     name: 'SohYeong Noh',
     role: 'Director of Art Centre Nabi and a board member of CC Korea',
+    line: 'images/title_underline.png',
     description:
           'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
     featuredImage: 'images/speaker_01 1 (2).svg',
@@ -33,6 +35,9 @@ speakersData.forEach((speaker) => {
                 <p class="p1">${speaker.role}</p>
             </li>
             <li>
+            <img src="${speaker.line}" alt="underline">
+            </li>
+            <li>
                 <p class="p2">${speaker.description}</p>
             </li>
         </ul>
@@ -48,6 +53,7 @@ const speakersData2 = [
     id: '02',
     name: 'Kilnam Chon',
     role: 'Berkman Professor',
+    line: 'images/title_underline.png',
     description:
           'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame ',
     featuredImage: 'images/speaker_01 1 (1).svg',
@@ -57,6 +63,7 @@ const speakersData2 = [
     id: '04',
     name: 'Julia Leda',
     role: 'President of Young Pirates of Europe',
+    line: 'images/title_underline.png',
     description:
           'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
     featuredImage: 'images/speaker_01 1 (3).svg',
@@ -66,6 +73,7 @@ const speakersData2 = [
     id: '05',
     name: 'Lila tretikov',
     role: 'Executive Director of the Wikimedia Foundation',
+    line: 'images/title_underline.png',
     description:
           'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
     featuredImage: 'images/speaker_01 1 (4).svg',
@@ -75,6 +83,7 @@ const speakersData2 = [
     id: '06',
     name: 'Ryan Merkley',
     role: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
+    line: 'images/title_underline.png',
     description:
           'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment',
     featuredImage: 'images/speaker_01 1 (5).svg',
@@ -94,6 +103,9 @@ speakersData2.forEach((speaker2) => {
             </li>
             <li>
                 <p class="p1">${speaker2.role}</p>
+            </li>
+            <li>
+            <img src="${speaker2.line}" alt="underline">
             </li>
             <li>
                 <p class="p2">${speaker2.description}</p>
